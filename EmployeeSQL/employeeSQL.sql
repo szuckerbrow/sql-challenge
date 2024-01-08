d3bfc2d9d7934bc87491768f503fe0727505a832
szuckerbrow <146024427+szuckerbrow@users.noreply.github.com>
2024-01-08 10:28:58 -0500

    updated code, left join instead of inner join

--- EmployeeSQL/employeeSQL.sql
+++ EmployeeSQL/employeeSQL.sql
@@ -70,7 +70,7 @@ CREATE TABLE salaries (
 -- List the employee number, last name, first name, sex, and salary of each employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees AS e
-JOIN salaries AS s ON (e.emp_no = s.emp_no);
+LEFT JOIN salaries AS s ON (e.emp_no = s.emp_no);
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
